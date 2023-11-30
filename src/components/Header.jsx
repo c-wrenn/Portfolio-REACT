fdedaa2c87108b9734d924adf200a10d96687b08
c-wrenn <crislynwren@yahoo.com>
2023-11-30 05:18:58 -0600

    "file structure added"

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+// Import Navigation component
+import Navigation from "./Navigation.jsx";
+
+// Header component
+const Header = () => {
+  return (
+    <header>
+      {/* <h1>Can add text here if needed</h1> */}
+      {/* Navigation component for the navigation links */}
+      < Navigation />
+    </header>
+  );
+};
+
+export default Header;
